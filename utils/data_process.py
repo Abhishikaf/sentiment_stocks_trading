bc26fe60e51f163cec5ac23fdb05600aec1347c5
Preston Hodsman <phodsman@yahoo.com>
2022-01-11 08:41:07 -0800

    updated main notebook to call alpaca function and to call first processing function for rolling average creation

--- utils/data_process.py
+++ utils/data_process.py
@@ -1,2 +1,10 @@
 import pandas as pd
 
+def return_rolling_averages(dataframe):
+    windows = [5, 10, 30, 100]
+    for w in windows:
+        dataframe[w]=dataframe["close"].rolling(w).mean()
+    return dataframe
+
+
+# def return_crossovers(dataframe)
