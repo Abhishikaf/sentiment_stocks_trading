0a7c0691fc8bfe5196cef78c4d37b4518c955fcf
Preston Hodsman <phodsman@yahoo.com>
2022-01-19 13:31:42 -0800

    added function to buy/sell a given percent of available money or shares on signal

--- utils/eval_functions.py
+++ utils/eval_functions.py
@@ -24,6 +24,7 @@ def results_trade_amount_nostop(start_money, close_df_test, trained_predictions,
             
     return money_on_hand_df, shares_df, value_on_hand_df
 
+
 # alternate test to add up all exercises, stops in place to prevent negative money or shares.
 def results_trade_amount_stops(start_money, close_df_test, trained_predictions, trade_amount):
     shares = 0
@@ -67,6 +68,31 @@ def buy_or_sell_all_if_available(start_money, close_df_test, trained_predictions
             money_on_hand = 0
     
     return money_on_hand_df, shares_df, value_on_hand_df
+
+
+
+# alternate strategy. spend trade_percent of available money on buy signal, sell trade_percent of available shares on sell signal.
+
+def buy_or_sell_trade_percent(trade_percent, start_money, close_df_test, trained_predictions):
+    shares = 0
+    money_on_hand = start_money
+    shares_df = pd.DataFrame(np.zeros(shape=trained_predictions.shape[0]), index=trained_predictions.index)
+    money_on_hand_df = pd.DataFrame(np.zeros(shape=trained_predictions.shape[0]), index=trained_predictions.index)
+    value_on_hand_df = pd.DataFrame(np.zeros(shape=trained_predictions.shape[0]), index=trained_predictions.index)
+    
+    for day in range(len(trained_predictions)):
+        shares_df.iloc[day][0] = shares
+        money_on_hand_df.iloc[day][0] = money_on_hand
+        value_on_hand_df.iloc[day][0] = (shares * close_df_test.iloc[day]["close"]) + money_on_hand
+        if (trained_predictions.iloc[day][0] == 0) & (shares > 0):
+            money_on_hand += (shares*trade_percent) * close_df_test.iloc[day]["close"]
+            shares = shares*(1-trade_percent)
+        elif (trained_predictions.iloc[day][0] == 1) & (money_on_hand > 0):
+            shares += (money_on_hand*trade_percent) / close_df_test.iloc[day]["close"]
+            money_on_hand = money_on_hand*(1-trade_percent)
+            
+    return money_on_hand_df, shares_df, value_on_hand_df
+
 
 
 
